371d85332e8fa9948796f221b97bab1e5dc25196
Simone Ingenito <sim.ingenito@gmail.com>
2026-06-12 21:58:10 +0200

    fix(be): restore database cleanup before each integration test

--- src/test/java/com/karateflow/backend/BaseIntegrationTest.java
+++ src/test/java/com/karateflow/backend/BaseIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.karateflow.backend;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -14,6 +17,14 @@ public abstract class BaseIntegrationTest {
 
     @Container
     protected static final MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:7.0"));
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    void cleanDatabase() {
+        mongoTemplate.getCollectionNames().forEach(mongoTemplate::dropCollection);
+    }
 
     @DynamicPropertySource
     static void setProperties(final DynamicPropertyRegistry registry) {
