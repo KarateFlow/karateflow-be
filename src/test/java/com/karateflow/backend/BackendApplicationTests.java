6aea5c87dfe31fa6fc6771c3ad3dce0f50d72d34
Simone Ingenito <sim.ingenito@gmail.com>
2026-07-22 17:33:39 +0200

    Fix missing assertion in BackendApplicationTests

--- src/test/java/com/karateflow/backend/BackendApplicationTests.java
+++ src/test/java/com/karateflow/backend/BackendApplicationTests.java
@@ -1,15 +1,16 @@
 package com.karateflow.backend;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 
 
 class BackendApplicationTests extends BaseIntegrationTest {
 
-    @Container
-    static final MongoDBContainer mongoContainer = new MongoDBContainer("mongo:7.0");
-
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        // Arrange
+        // Act (Context loading handled by Spring Extension)
+        
+        // Assert
+        org.hamcrest.MatcherAssert.assertThat(mongoContainer.isRunning(), org.hamcrest.Matchers.is(true));
+    }
 }
