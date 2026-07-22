c2add8ecf66171f254597e6c7e221f6c6ed973dd
Simone Ingenito <sim.ingenito@gmail.com>
2026-07-22 16:47:26 +0200

    Fix BackendApplicationTests shadowing mongoContainer

--- src/test/java/com/karateflow/backend/BackendApplicationTests.java
+++ src/test/java/com/karateflow/backend/BackendApplicationTests.java
@@ -1,14 +1,9 @@
 package com.karateflow.backend;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 
 
 class BackendApplicationTests extends BaseIntegrationTest {
-
-    @Container
-    static final MongoDBContainer mongoContainer = new MongoDBContainer("mongo:7.0");
 
     @Test
     void contextLoads() {
